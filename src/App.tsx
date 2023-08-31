0aa8a048956c71ea985e4ab5c33b7618c0fffc52
Chaveguapo <catakeas@gmail.com>
2023-08-31 16:21:03 +0200

    Values on Uppercase

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const handleSubmit = (event: { preventDefault: () => void }) => {
-    event.preventDefault();
-    alert("You have submitted the form");
+  const [enteredText, setEnteredText] = useState("");
+  const [submittedText, setSubmittedText] = useState("");
+  const textChangeHandler = (i: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setEnteredText(i.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSubmittedText(enteredText);
+    setEnteredText(" ");
   };
 
   return (
-    <div className="wrapper">
+    <div className="container">
       <h1>Enter your Name</h1>
       <form onSubmit={handleSubmit}>
         <fieldset>
           <label>
-            <p>Name</p>
-            <input name="name" />
+            <p>First Name</p>
+            <input
+              type="text"
+              value={enteredText}
+              onChange={textChangeHandler}
+              className="input"
+            />
           </label>
         </fieldset>
-        <button type="submit">Submit</button>
+        <button type="submit" className="btn">
+          Submit
+        </button>
       </form>
+
+      {submittedText && <p>You just typed {submittedText.toUpperCase()}</p>}
     </div>
   );
 }
